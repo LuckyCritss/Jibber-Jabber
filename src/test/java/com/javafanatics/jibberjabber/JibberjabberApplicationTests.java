0877302e5a1546d2c11deb97d9c484852ae2af78
gregdc@webtrace.be <gregdc01>
2021-06-18 16:26:50 +0200

    initial commit

---
+++ src/test/java/com/javafanatics/jibberjabber/JibberjabberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafanatics.jibberjabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberjabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
